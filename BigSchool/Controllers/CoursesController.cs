50eb87c55c2dec58095fad3f93ad98e191576728
Anhtuan481993@gmail.com <Anhtuan481993@gmail.com>
2019-04-03 11:09:19 +0700

    Thay doi lan 2

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BigSchool.Models;
+using BigSchool.ViewModels;
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        // GET: Course
+        //adsfafa
+        ///fsdgrreycghb,jhm
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
