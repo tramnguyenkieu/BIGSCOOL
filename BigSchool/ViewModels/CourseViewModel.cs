1f5dc0f89f362a852622b6f667db9d5d455566ee
Anhtuan481993@gmail.com <Anhtuan481993@gmail.com>
2019-04-03 12:04:46 +0700

    thay doi lan 3

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 using System.Linq;
 using System.Web;
 using BigSchool.Models;
@@ -7,10 +9,15 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
-       
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
---
+++ BigSchool/ViewModels/requriedAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BigSchool.ViewModels
+{
+    internal class requriedAttribute : Attribute
+    {
+    }
+}
