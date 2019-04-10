3a3eff72fd4eef56fb4a7190cc1d2e18910d2cc8
Anhtuan481993@gmail.com <Anhtuan481993@gmail.com>
2019-04-11 00:22:05 +0700

    tdlc

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -22,11 +22,11 @@ namespace BigSchool.Controllers
         {
             var userId = User.Identity.GetUserId();
             if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
-                return BadRequest("The attendance already exists!");
+                return BadRequest("The Attendance already exists!");
             var attendance = new Attendance
             {
                 CourseId = attendanceDto.CourseId,
-                AttendeeId = User.Identity.GetUserId()
+                AttendeeId = userId
 
             };
             _dbContext.Attendances.Add(attendance);
